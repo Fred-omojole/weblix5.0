fc5eeb62af4b966760a0daad4b34e4933f8a413a
freddy.dev <omoyeleomojole2004@gmail.com>
2024-10-19 10:55:32 +0100

    prismic  installation

--- app/(Home)/Hero.tsx
+++ app/(Home)/Hero.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Hero = () => {
-  return <section className="min-h-screen">Hero</section>;
+  return (
+    <section className="min-h-screen pt-10 ">
+      <div className="container w-[1440] mx-auto">
+        <div className="flex flex-col"> Hello</div>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Hero from "./(Home)/Hero";
 export default function Home() {
   return (
     <main className="min-h-screen">
-      <div className=" w-full  bg-dot-black/[0.15] relative">
+      <div className=" w-full  bg-dot-black/[0.1] relative">
         <Hero />
       </div>
       <div className="absolute  inset-0 flex items-center justify-center  [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
