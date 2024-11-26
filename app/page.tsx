6e27af18f874aef55a179e2c6e6ebebb7290da13
freddy.dev <omoyeleomojole2004@gmail.com>
2024-11-26 17:16:05 +0100

    services incomplete

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
     <main className="min-h-screen">
       <div className=" w-full  bg-dot-black/[0.1] relative">
         <Hero />
-        <NavbarDemo />
+        {/* <NavbarDemo /> */}
       </div>
       <div className="-z-10 absolute inset-0 flex items-center justify-center  [mask-image:radial-gradient(to_bottom,black_50%,transparent_100%))]"></div>
       <About />
